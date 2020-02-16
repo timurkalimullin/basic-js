b756b644942e1385096c08dd0cfd8bb4bacaf740
Timur Kalimullin <timur_kalimullin@list.ru>
2020-02-16 19:12:43 +0500

    feat: count cats resolved

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let count = 0;
+   for (let i in arr) {
+    for (let j of arr[i]) {
+      if (j =="^^") {
+        count++;
+      }
+    }
+   }
+   return count;
 };
+
