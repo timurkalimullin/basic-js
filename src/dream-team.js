bf74049a61a68b536fb31e04dbdc8125d1d628b7
Timur Kalimullin <timur_kalimullin@list.ru>
2020-02-17 16:35:27 +0500

    feat: dream-team task resolved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  let result =[];
+
+  if (Array.isArray(arr)) {
+    for (let i of arr) {
+      if (typeof(i) == "string" && /^[a-z\s_]+$/i.test(i))  {
+          result.push(i);
+      }
+    } 
+  } else {return false}
+  let res=[];
+  for (i of result) {
+      res.push((i.match(/\S/)[0]))
+  }
+  res = res.map(i => i.toUpperCase());
+
+  return res.sort().join("");
 };
