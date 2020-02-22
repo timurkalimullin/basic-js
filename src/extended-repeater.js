a60737fc0549394e51cd6bd637671bfa7b918f0f
Timur Kalimullin <timur_kalimullin@list.ru>
2020-02-22 11:07:09 +0500

    feat: resolve extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,25 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str,{repeatTimes, separator = "+", addition, additionRepeatTimes, additionSeparator = "|"}){
+    let addBlock = [],
+        finalBlock = [];
+    
+
+    if (String(addition).length>0) {
+        if (additionRepeatTimes>0) {
+            while (additionRepeatTimes) {
+                addBlock.push(addition===null?"null":String(addition));
+                additionRepeatTimes--;
+            } 
+        } else { addBlock.push(addition)}
+    }
+
+    str = (str === null?"null":String(str)) + addBlock.join(additionSeparator);
+    
+    if (repeatTimes>0) {
+        while (repeatTimes) {
+            finalBlock.push(str);
+            repeatTimes--;
+        }
+    } else {finalBlock.push(str)};
+    
+    return finalBlock.join(separator)
 };
-  
